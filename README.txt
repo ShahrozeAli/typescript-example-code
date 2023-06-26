5a666d2b1afe0c5c6ec2d71c1e60c48e2c6ffff3
ShahrozeAppify <shahroze.ali@appifytech.com>
2023-06-26 16:49:59 +0500

    compiling ts file to js

---
+++ README.txt
@@ -0,0 +1,2 @@
+compile Typescript code to Js code
+Run: tsc [typescript filename]
---
+++ main.ts
@@ -0,0 +1,7 @@
+function log (message) {
+    console.log("message => ", message);
+}
+
+var message = "This is Typescript file";
+
+log(message);
