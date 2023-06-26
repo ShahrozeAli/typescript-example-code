8be32cd58598d4d260c10a7af060759a91acb6c6
ShahrozeAppify <shahroze.ali@appifytech.com>
2023-06-26 17:47:01 +0500

    constructor

--- class.ts
+++ class.ts
@@ -1,6 +1,12 @@
 class Point {
   x: number;
   y: number;
+  constructor(x?: number, y?: number) {
+    // in ts we can't have multiple constructors
+    //making values optional, so object doesn't throw error if params are missing
+    this.x = x !== undefined ? x : 0;
+    this.y = y !== undefined ? y : 0;
+  }
 
   draw() {
     console.log("X = ", this.x, " Y = ", this.y);
@@ -10,8 +16,9 @@ class Point {
 }
 
 // let point: Point = new Point(); //initializes memory to it
-let point = new Point(); //initializes memory to it
+// let point = new Point(); //initializes memory to it
+let point = new Point(1, 2); //initializes memory to it
 
-point.x = 1;
-point.y = 2;
+// point.x = 1;
+// point.y = 2;
 point.draw();
